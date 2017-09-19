8ff6ed831c2e8ec168e442f9600954eaf08d53fd
x1ah <x1ahgxq@gmail.com>
2017-09-19 11:52:04 +0800

    retry decorate

---
+++ retry.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import requests
+
+
+def retries(times=3):
+    def wrap(func):
+        def r(*args, **kw):
+            t = 0
+            while t < times:
+                try:
+                    func(*args, **kw)
+                except requests.exceptions.ConnectTimeout:
+                    t += 1
+                else:
+                    return
+        return r
+    return wrap
+
+
+@retries(times=4)
+def fetch_google():
+    print('retry')
+    resp = requests.get('https://google.com', timeout=0.1)
+    return resp
+
+
+if __name__ == "__main__":
+    fetch_google()
